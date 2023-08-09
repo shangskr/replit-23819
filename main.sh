d9078b9991a4ec3c1303141a4e49f464cf5a3103
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2023-08-09 18:40:56 +0800

    配置main.sh启动时自动更新

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 防休眠命令
 while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
+# 更新Memos
+rm -rf memos && wget -O memos https://github.com/SinzMise/memos-on-replit/raw/main/memos.moe
+
 # 启动Memos
 echo "run memos >>> $(date '+%Y-%m-%d %H:%M:%S')"
 
